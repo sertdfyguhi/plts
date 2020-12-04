5709e415118b8635a6c80fc37dba4cf52a39633a
sertdfyguhi <71869572+sertdfyguhi@users.noreply.github.com>
2020-12-05 07:37:53 +0800

    optimize

--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from sys import argv
 
 def run(text):
-        stack = []
-        toprint = ''
-        e = False
-        a = False
-        for idx, letter in enumerate(text):
-            if a == False:
-                if letter == '?':
-                    a = True
-                    stack.append(text[idx + 1])
-                elif letter == '%':
-                    a = True
-                    stack.insert(0, text[idx + 1])
-                elif letter == '&':
-                    toprint += stack[-1] + '\n'
-                elif letter == '$':
-                    toprint += stack[0] + '\n'
-                elif letter == '#':
-                    toprint += ''.join(stack) + '\n'
-                elif letter == '@':
-                    toprint += ''.join(stack)[::-1] + '\n'
-                elif letter == '<':
-                    del stack[0]
-                elif letter == '>':
-                    del stack[-1]
-                else:
-                    print(f'{str(idx + 1)}: error')
-                    e = True
+    stack = []
+    toprint = ''
+    e = False
+    a = False
+    for idx, letter in enumerate(text):
+        if a == False:
+            if letter == '?':
+                a = True
+                stack.append(text[idx + 1])
+            elif letter == '%':
+                a = True
+                stack.insert(0, text[idx + 1])
+            elif letter == '&':
+                toprint += stack[-1] + '\n'
+            elif letter == '$':
+                toprint += stack[0] + '\n'
+            elif letter == '#':
+                toprint += ''.join(stack) + '\n'
+            elif letter == '@':
+                toprint += ''.join(stack)[::-1] + '\n'
+            elif letter == '<':
+                del stack[0]
+            elif letter == '>':
+                del stack[-1]
             else:
-                a = False
-        if e == False:
-            s = toprint.rsplit('\n', 1)
-            print(''.join(s))
+                print(f'{str(idx + 1)}: error')
+                e = True
+                break
+        else:
+            a = False
+    if e == False:
+        s = toprint.rsplit('\n', 1)
+        print(''.join(s))
 
 if len(argv) > 1:
     with open(argv[1], 'r') as f:
         text = f.read()
-
     run(text)
 else:
     while True:
