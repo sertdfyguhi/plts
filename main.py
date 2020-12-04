4bce41695583e74164958282c205c0a3f9d06a47
sertdfyguhi <71869572+sertdfyguhi@users.noreply.github.com>
2020-12-04 19:20:21 +0800

    optimise????

--- main.py
+++ main.py
@@ -1,43 +1,6 @@
 from sys import argv
 
-if len(argv) > 1:
-    with open(argv[1], 'r') as f:
-        text = f.read()
-
-    stack = []
-    toprint = ''
-    e = False
-    a = False
-    for idx, letter in enumerate(text):
-        if a == False:
-            if letter == '?':
-                a = True
-                stack.append(text[idx + 1])
-            elif letter == '%':
-                a = True
-                stack.insert(0, text[idx + 1])
-            elif letter == '&':
-                toprint += stack[-1] + '\n'
-            elif letter == '$':
-                toprint += stack[0] + '\n'
-            elif letter == '#':
-                toprint += ''.join(stack) + '\n'
-            elif letter == '@':
-                toprint += ''.join(stack)[::-1] + '\n'
-            elif letter == '<':
-                del stack[0]
-            elif letter == '>':
-                del stack[-1]
-            else:
-                print(f'{str(idx + 1)}: error')
-                e = True
-        else:
-            a = False
-    if e == False:
-        print(toprint)
-else:
-    while True:
-        text = input('> ')
+def run(text):
         stack = []
         toprint = ''
         e = False
@@ -70,3 +33,13 @@ else:
         if e == False:
             s = toprint.rsplit('\n', 1)
             print(''.join(s))
+
+if len(argv) > 1:
+    with open(argv[1], 'r') as f:
+        text = f.read()
+
+    run(text)
+else:
+    while True:
+        text = input('> ')
+        run(text)
